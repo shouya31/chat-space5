6eaca529c5d736070ec40edfec29ceb53daa8568
shouya31 <shouya19980331@gmail.com>
2018-12-12 20:05:29 +0900

    ユーザー編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 	root 'messages#index'
 	resources :messages, only: [:index, :create]
+	resources :users, only: [:edit, :update]
 	resources :tests
 end
