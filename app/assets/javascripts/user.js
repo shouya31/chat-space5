e32a8f589572768137654b88ab16c44095c82001
shouya31 <shouya19980331@gmail.com>
2018-12-16 19:44:31 +0900

    user.jsの修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,7 @@
 $(function(){
 	　 var search_list = $("#user-search-result");
    
-    function appendUser(user) {
+  function appendUser(user) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user.name}</p>
                     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -9,7 +9,7 @@ $(function(){
     search_list.append(html);
   };
 
-   function appendNoUser() {
+  function appendNoUser() {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>一致するユーザーはいません</p>
                 </div>`
@@ -17,7 +17,7 @@ $(function(){
   };
 
   function buildHTML(id, name) {
-    var html = `<div class="chat-group-user clearfix" id=chat-group-user-${id}>
+    var html = `<div class="chat-group-user clearfix">
                   <input type="hidden" name="group[user_ids][]" value="${id}">
                   <p class="chat-group-user__name">${name}</p>
                   <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a>
@@ -28,7 +28,6 @@ $(function(){
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log("はははははははー")
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -41,7 +40,6 @@ $(function(){
       if (users.length !== 0 && input.length !== 0) {
         users.forEach(function(user) {
           appendUser(user);
-          console.log("ウェーーーーーーイ")
         });
       } 
       else {
@@ -54,17 +52,17 @@ $(function(){
     });
 
   });
-   $(".user-search-result").on('click','.user-search-add', function() {
-    var id = $(this).data('user-id');
-    var name = $(this).data('user-name');
-    var insertHTML = buildHTML(id, name);
-    $('.chat-group-users').append(insertHTML);
-    $(this).parent('.chat-group-user').remove();
+   $("#user-search-result").on('click','.user-search-add', function() {
+      var id = $(this).data('user-id');
+      var name = $(this).data('user-name');
+      var insertHTML = buildHTML(id, name);
+      $('.chat-group-users').append(insertHTML);
+      $(this).parent('.chat-group-user').remove();
   });
 
   $(".chat-group-users").on('click', '.user-search-remove', function() {
     var id = $(this).data('user-id');
-    $(`#chat-group-user-${id}`).remove();
+    $(this).parent('.chat-group-user').remove();
   });
 	
 });
